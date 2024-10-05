f3cd63095a837930f65d2ab6e2e913c986d302d0
lucvllm <luc.villermain@gmail.com>
2024-10-05 18:28:13 +0200

    Update crypto_am_V1.py

--- crypto_am_V1.py
+++ crypto_am_V1.py
@@ -17,8 +17,8 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-username_entered = st.secrets["username"]    #["database"]
-password_entered = st.secrets["password"]    #["database"]
+username_entered = st.secrets["username"]   
+password_entered = st.secrets["password"]   
 
 
 st.title('CRYPTAM')
